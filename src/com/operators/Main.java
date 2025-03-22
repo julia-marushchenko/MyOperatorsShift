9bc78b68eca57884f82325122a15ac6b4d9ac4d0
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-03-23 00:34:31 +0100

    Add files via upload
    Commiting class Main

---
+++ src/com/operators/Main.java
@@ -0,0 +1,16 @@
+package com.operators;
+
+// Class
+public class Main {
+
+    // Method main to run java program
+    public static void main(String[] args) {
+
+        int x = -4;
+        System.out.println(x >> 1); // becomes -2
+
+        int y = 4;
+        System.out.println(y >> 1); // becomes 2
+
+    }
+}
